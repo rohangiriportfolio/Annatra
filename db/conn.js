6642f2753f623aff236d575c9f149f709e8e173d
Rohan Giri <rohangiri.portfolio@gmail.com>
2025-09-07 19:05:07 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,5 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
-
 const DB = process.env.MONGODB_URI;
-// const DB = 'mongodb://127.0.0.1:27017/vendorDealer';
 
 mongoose.connect(DB)
   .then(() => console.log("MongoDB Connected"))
