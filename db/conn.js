1c573370622020ceb7b0a01a90df13d71dc5322e
Rohan Giri <rohangiri.portfolio@gmail.com>
2025-09-07 13:07:13 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -4,14 +4,7 @@ const mongoose = require('mongoose');
 const DB = process.env.MONGODB_URI;
 // const DB = 'mongodb://127.0.0.1:27017/vendorDealer';
 
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, // Adjust timeout as needed
-};
-
-mongoose.connect(DB, options)
+mongoose.connect(DB)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => {
     console.error("MongoDB connection error:", err);
