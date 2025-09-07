4810bb116bd77194bca8729f13606f307573956e
Rohan Giri <rohangiri.portfolio@gmail.com>
2025-09-07 11:51:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("./db/conn.js");
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 const userdb = require("./model/user");
 const dealerdb = require("./model/dealer");
